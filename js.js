b604428f076156ee1ca343ac5138ce10fb7e3a3e
Alreh <rehn.aleksi@gmail.com>
2017-11-20 06:24:04 +0200

    testing more

--- js.js
+++ js.js
@@ -1,35 +1,3 @@
-function browser(){
-    // Opera 8.0+
-    var isOpera = (!!window.opr && !!opr.addons) || !!window.opera || navigator.userAgent.indexOf(' OPR/') >= 0;
-
-    // Firefox 1.0+
-    var isFirefox = typeof InstallTrigger !== 'undefined';
-
-    // Safari 3.0+ "[object HTMLElementConstructor]" 
-    var isSafari = /constructor/i.test(window.HTMLElement) || (function (p) { return p.toString() === "[object SafariRemoteNotification]"; })(!window['safari'] || safari.pushNotification);
-
-    // Internet Explorer 6-11
-    var isIE = /*@cc_on!@*/false || !!document.documentMode;
-
-    // Edge 20+
-    var isEdge = !isIE && !!window.StyleMedia;
-
-    // Chrome 1+
-    var isChrome = !!window.chrome && !!window.chrome.webstore;
-
-    // Blink engine detection
-    var isBlink = (isChrome || isOpera) && !!window.CSS;
-
-var output = 'Checking browser used for testing:<hr>';
-output += 'isFirefox: ' + isFirefox + '<br>';
-output += 'isChrome: ' + isChrome + '<br>';
-output += 'isSafari: ' + isSafari + '<br>';
-output += 'isOpera: ' + isOpera + '<br>';
-output += 'isIE: ' + isIE + '<br>';
-output += 'isEdge: ' + isEdge + '<br>';
-output += 'isBlink: ' + isBlink + '<br><br>';
-document.getElementById("first").innerHTML = output;
-}
 
 function calculationExample(){
 	var x = 100;
@@ -38,8 +6,6 @@ function calculationExample(){
 	checkSum(z);
 }
 
-
-@test
 function checkSum(z){
 	if(z == 350){
 		document.getElementById("second").innerHTML = "calculationExample result was valid, check passed!";
